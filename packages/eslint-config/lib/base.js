9fb309a0de983c8f0143d59230c95543af7f3274
Claudio Rimann <claudio@haptiq.studio>
2026-07-01 00:05:21 +0200

    Add @stylistic/eslint-plugin as a dependency + load in base config

--- packages/eslint-config/lib/base.js
+++ packages/eslint-config/lib/base.js
@@ -4,12 +4,16 @@
  * Extend with `react` or `node` for framework/runtime-specific rules.
  */
 import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 import globals from 'globals'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	js.configs.recommended,
 	{
+		plugins: {
+			'@stylistic': stylistic,
+		},
 		languageOptions: {
 			globals: {
 				...globals.browser,
